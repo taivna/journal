c87bb1ef71586c2fdbff871acbe19bfd84878893
taivna <taivna86@gmail.com>
2017-04-22 14:35:03 +0800

    StudentAddActivity's continue and GuardianAddActivity's save buttons are now disabled if any of the edittext fields is empty

--- app/src/main/java/se315/journal/GuardianAddActivity.java
+++ app/src/main/java/se315/journal/GuardianAddActivity.java
@@ -1,17 +1,23 @@
 package se315.journal;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.TypedValue;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class GuardianAddActivity extends AppCompatActivity
 {
+    Button saveBtn;
+    Student student;
     DBHelper dbHelper;
     EditText register, name, surName, phoneNumber, eMail, relation, studentRegister;
-    Student student;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,6 +26,7 @@ public class GuardianAddActivity extends AppCompatActivity
         setContentView(R.layout.activity_guardian_add);
         getSupportActionBar().setTitle(R.string.gua_title);
 
+        saveBtn = (Button) findViewById(R.id.gua_btn_save);
         name = (EditText) findViewById(R.id.gua_et_fn);
         surName = (EditText) findViewById(R.id.gua_et_ln);
         phoneNumber = (EditText) findViewById(R.id.gua_et_phone);
@@ -33,6 +40,142 @@ public class GuardianAddActivity extends AppCompatActivity
         studentRegister.setText(student.getRegister());
 
         dbHelper = new DBHelper(this);
+
+        saveBtn.setEnabled(false);
+        saveBtn.setBackgroundColor(Color.GRAY);
+
+        name.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+                enableSaveBtn();
+            }
+        });
+
+        surName.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+                enableSaveBtn();
+            }
+        });
+
+        phoneNumber.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+                enableSaveBtn();
+            }
+        });
+
+        eMail.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+                enableSaveBtn();
+            }
+        });
+
+        relation.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+                enableSaveBtn();
+            }
+        });
+
+        register.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+                enableSaveBtn();
+            }
+        });
+
+        studentRegister.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+                enableSaveBtn();
+            }
+        });
     }
 
     public void switchToAddStudent(View view)
@@ -60,5 +203,65 @@ public class GuardianAddActivity extends AppCompatActivity
             Toast.makeText(getApplicationContext(), "Шинэ сурагч бүртгэлд нэмэгдлээ", Toast.LENGTH_SHORT).show();
         }
         finish();
+    }
+
+    public int getThemeColor()
+    {
+        TypedValue typedValue = new TypedValue();
+        getTheme().resolveAttribute(R.attr.colorPrimary, typedValue, true);
+        int color = typedValue.data;
+        return color;
+    }
+
+    public void enableSaveBtn()
+    {
+        boolean enabled = true;
+
+        if(name.getText().toString().length() == 0)
+        {
+            enabled = false;
+        }
+
+        if(surName.getText().toString().length() == 0)
+        {
+            enabled = false;
+        }
+
+        if(phoneNumber.getText().toString().length() == 0)
+        {
+            enabled = false;
+        }
+
+        if(eMail.getText().toString().length() == 0)
+        {
+            enabled = false;
+        }
+
+        if(relation.getText().toString().length() == 0)
+        {
+            enabled = false;
+        }
+
+        if(register.getText().toString().length() == 0)
+        {
+            enabled = false;
+        }
+
+        if(studentRegister.getText().toString().length() == 0)
+        {
+            enabled = false;
+        }
+
+        if(enabled)
+        {
+            saveBtn.setEnabled(true);
+            saveBtn.setBackgroundColor(getThemeColor());
+        }
+        else
+        {
+            saveBtn.setEnabled(false);
+            saveBtn.setBackgroundColor(Color.GRAY);
+        }
+
     }
 }
--- app/src/main/java/se315/journal/StudentAddActivity.java
+++ app/src/main/java/se315/journal/StudentAddActivity.java
@@ -1,19 +1,27 @@
 package se315.journal;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.TypedValue;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 public class StudentAddActivity extends AppCompatActivity
 {
-    EditText register, name, surName, phoneNumber, eMail, address, enrolledYear;
+    Button continueBtn;
     RadioGroup gender;
     String radioButtonText = "";
+    EditText register, name, surName, phoneNumber, eMail, address, enrolledYear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -30,6 +38,207 @@ public class StudentAddActivity extends AppCompatActivity
         register = (EditText) findViewById(R.id.stadd_et_register);
         enrolledYear = (EditText) findViewById(R.id.stadd_et_enrolldate);
         gender = (RadioGroup) findViewById(R.id.stadd_radioGroup);
+
+        continueBtn = (Button) findViewById(R.id.stadd_btn_cont);
+        continueBtn.setEnabled(false);
+        continueBtn.setBackgroundColor(Color.GRAY);
+
+        //setting up textchangedlistener for every edittext
+        name.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+                enableContinueBtn();
+            }
+        });
+
+        surName.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+                enableContinueBtn();
+            }
+        });
+
+        phoneNumber.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+                enableContinueBtn();
+            }
+        });
+
+        eMail.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+                enableContinueBtn();
+            }
+        });
+
+        address.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+                enableContinueBtn();
+            }
+        });
+
+        register.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+                enableContinueBtn();
+            }
+        });
+
+        enrolledYear.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+                enableContinueBtn();
+            }
+        });
+    }
+
+    public void enableContinueBtn()
+    {
+        boolean enabled = true;
+
+        if(name.getText().toString().length() == 0)
+        {
+            enabled = false;
+        }
+
+        if(surName.getText().toString().length() == 0)
+        {
+            enabled = false;
+        }
+
+        if(phoneNumber.getText().toString().length() == 0)
+        {
+            enabled = false;
+        }
+
+        if(eMail.getText().toString().length() == 0)
+        {
+            enabled = false;
+        }
+
+        if(address.getText().toString().length() == 0)
+        {
+            enabled = false;
+        }
+
+        if(register.getText().toString().length() == 0)
+        {
+            enabled = false;
+        }
+
+        if(enrolledYear.getText().toString().length() == 0)
+        {
+            enabled = false;
+        }
+
+        if(enabled)
+        {
+            continueBtn.setEnabled(true);
+            continueBtn.setBackgroundColor(getThemeColor());
+        }
+        else
+        {
+            continueBtn.setEnabled(false);
+            continueBtn.setBackgroundColor(Color.GRAY);
+        }
     }
 
     public void switchToAddGuardian(View view)
@@ -80,5 +289,13 @@ public class StudentAddActivity extends AppCompatActivity
         eMail.setText("");
         address.setText("");
         enrolledYear.setText("");
+    }
+
+    public int getThemeColor()
+    {
+        TypedValue typedValue = new TypedValue();
+        getTheme().resolveAttribute(R.attr.colorPrimary, typedValue, true);
+        int color = typedValue.data;
+        return color;
     }
 }
